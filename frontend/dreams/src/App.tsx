52e51db0c38953f906a22d776185aed2c97a5084
ionleu <leuionofficial@gmail.com>
2023-07-03 09:59:49 +0300

    tailwind, styled-components, added auth components

--- frontend/dreams/src/App.tsx
+++ frontend/dreams/src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen.tsx";
+import SignupScreen from "./screens/SignupScreen.tsx";
+
+
 const App = () => {
-    return <h1>Dream project</h1>
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<HomeScreen />}/>
+                <Route path='/login' element={<LoginScreen />}/>
+                <Route path='/signup' element={<SignupScreen />}/>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
---
+++ frontend/dreams/src/screens/HomeScreen.tsx
@@ -0,0 +1,9 @@
+const HomeScreen = () => {
+    return (
+        <>
+            <h1>Home screen</h1>
+        </>
+    )
+}
+
+export default HomeScreen
