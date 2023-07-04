ce0a340cb489da240442310d02ed22bbf2df7c7a
edward375 <rotarueduard91@gmail.com>
2023-07-04 13:34:28 +0300

    done

---
+++ src/components/BeOurHero/Behero.tsx
@@ -0,0 +1,21 @@
+
+const BeHero = () => {
+    return (
+        <div className='containerBeHero'>
+            <h1 className='titleBeHero'>Be our hero</h1>
+            <div className= 'contentBeHero'>
+                <div className= 'containerDescription'>
+                    <h3 className='titleConDesc'>Dreamer</h3>
+                    <p className= 'textDescription'>Dreamer is a person who gives for other dreams and receive back twice for it’s own dream.</p>
+                </div>
+                <div className= 'containerDescription'>
+                    <h3 className='titleConDesc'>Dream Angel</h3>
+                    <p className= 'textDescription'>Dream Angel is a person who give to others and collect fulfilled dreams for his future dream or further donation.</p>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default BeHero
