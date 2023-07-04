a523faa6ca2466e638e4c0d786ad9536a0a70c6a
MortyNiners <mariuss.sirbu@yandex.com>
2023-07-04 22:12:21 +0300

    Created Wings Donation and Securely Donation also optimized for mobile

--- frontend/dreams/src/App.tsx
+++ frontend/dreams/src/App.tsx
@@ -1,19 +1,28 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomeScreen from "./screens/HomeScreen";
-import LoginScreen from "./screens/LoginScreen";
-import SignupScreen from "./screens/SignupScreen";
+// import {BrowserRouter, Routes, Route} from "react-router-dom";
+// import HomeScreen from "./screens/HomeScreen";
+// import LoginScreen from "./screens/LoginScreen";
+// import SignupScreen from "./screens/SignupScreen";
+import Wings_donation from "./components/Wings Donation/wings_donation";
+import Securely_donation from "./components/Securely Donation/securely_donation";
 
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<HomeScreen />}/>
-                <Route path='/login' element={<LoginScreen />}/>
-                <Route path='/signup' element={<SignupScreen />}/>
-            </Routes>
-        </BrowserRouter>
-    )
+        <>
+            <Securely_donation />
+            <Wings_donation />
+        </>
+        // <BrowserRouter>
+        //     <Routes>
+        //         {/*<Route path='/' element={<HomeScreen/>}/>*/}
+        //         {/*<Route path='/login' element={<LoginScreen/>}/>*/}
+        //         {/*<Route path='/signup' element={<SignupScreen/>}/>*/}
+        //
+        //     </Routes>
+        // </BrowserRouter>
+
+
+)
 }
 
 export default App
