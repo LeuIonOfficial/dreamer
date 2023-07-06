c6a95b134cbb2fd56997fc20e6fbca99be0ab7a3
Daniel20 <tatarud24@gmail.com>
2023-07-06 13:45:54 +0300

    easier-project and project-purpose

--- frontend/dreams/src/components/purpose/Easier.tsx
+++ frontend/dreams/src/components/purpose/Easier.tsx
@@ -1,54 +1,118 @@
 import './ProjectPurpose.css'
+import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 
+const ContainerEasier = styled.div`
+  background-image: url("./img/Background4.ce0bbc248e40244237da.png");
+  height: 775px;
+  padding: 82px 160px;
+  background-repeat: no-repeat;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-around;
+  font-family: 'Space Grotesk', sans-serif;
+`;
+
+const TextHeadingEasier = styled.h1`
+  color: white;
+  font-size: 64px;
+`;
+
+const SectionEasier = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: start;
+  width: 100%;
+`;
+
+const ColumnEasier = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+const RowEasier = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 60px;
+`;
+
+const NumberCircleEasier = styled.div`
+  border: 1px solid #fff;
+  border-radius: 50%;
+  color: #fff;
+  font-size: 27px;
+  font-weight: 700;
+  line-height: 82px;
+  text-align: center;
+  width: 82px;
+`;
+
+const TextContentEasier = styled.p`
+  font-size: 23px;
+  color: white;
+  line-height: 1;
+  margin-left: 20px;
+  width: 270px;
+`;
+
+const ButtonEasier = styled.button`
+  background: linear-gradient(297.06deg , #f8ed84 23.88% , #f5e0ff 66.2% , #84fad5 109.31%);
+  border-radius: 43px;
+  height: 72px;
+  width: 350px;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TextButtonEasier = styled.span`
+  color: #262626;
+  font-size: 16px;
+  letter-spacing: 0.14em;
+`;
 
 
 const Easier = () => {
 
     const navigate = useNavigate();
-    const handleButtonClick = () => {
-        navigate('/signup');
-    };
+
 
 
     return (
-        <div className='container-easier'>
-        <h1 className='text-heading-easier'>It's easier than you think</h1>
-    <div className='section-easier'>
+        <ContainerEasier>
+            <TextHeadingEasier>It's easier than you think</TextHeadingEasier>
+            <SectionEasier>
+                <ColumnEasier>
+                    <RowEasier>
+                        <NumberCircleEasier>1</NumberCircleEasier>
+                        <TextContentEasier>Set your dream</TextContentEasier>
+                    </RowEasier>
+                    <RowEasier>
+                        <NumberCircleEasier>2</NumberCircleEasier>
+                        <TextContentEasier>Start giving a half of your Dream</TextContentEasier>
+                    </RowEasier>
+                </ColumnEasier>
+                <ColumnEasier>
+                    <RowEasier>
+                        <NumberCircleEasier>3</NumberCircleEasier>
+                        <TextContentEasier>Receive back twice for your dream</TextContentEasier>
+                    </RowEasier>
+                    <RowEasier>
+                        <NumberCircleEasier>4</NumberCircleEasier>
+                        <TextContentEasier>Live your dream. Keep Dreaming!</TextContentEasier>
+                    </RowEasier>
+                </ColumnEasier>
+            </SectionEasier>
+            <ButtonEasier onClick={() => navigate("/signup")}>
+                <TextButtonEasier>{`Try now`}</TextButtonEasier>
+            </ButtonEasier>
+        </ContainerEasier>
 
-        <div className='column-easier'>
-
-            <div className='row-easier'>
-                <div className='number-circle-easier'>1</div>
-                <p className='text-content-easier'>Set your dream</p>
-            </div>
-
-            <div className='row-easier'>
-                <div className='number-circle-easier'>2</div>
-                <p className='text-content-easier'
-                >Start giving a half of your Dream</p>
-            </div>
-        </div>
-
-        <div className='column-easier'>
-            <div className='row-easier'>
-                <div className='number-circle-easier'>3</div>
-                <p className='text-content-easier'>Receive back twice for your dream</p>
-            </div>
-
-            <div className='row-easier'>
-                <div className='number-circle-easier'>4</div>
-                <p className='text-content-easier'
-                >Live your dream. Keep Dreaming !</p>
-            </div>
-        </div>
-    </div>
-
-    <div type="submit"  className='btn-easier' onClick={handleButtonClick}>
-        <span className='text-button-easier'>Try now </span>
-    </div>
-
-</div>
 
     );
 
--- frontend/dreams/src/components/purpose/ProjectPurpose.tsx
+++ frontend/dreams/src/components/purpose/ProjectPurpose.tsx
@@ -1,5 +1,61 @@
 import './ProjectPurpose.css'
+import styled from "styled-components";
 import {useNavigate} from "react-router-dom";
+
+const ProjectPurposeStyle = styled.div`
+  height: 809px;
+  padding: 90px 100px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid #f1f1f1;
+  font-family: 'Space Grotesk', sans-serif;
+`;
+
+const ProjectSectionPurpose = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+  height: 518px;
+  margin-right: 35px;
+`;
+
+const HeadingPurpose = styled.h1`
+  color: black;
+  font-size: 64px;
+  line-height: 1;
+  margin-bottom: -40px;
+`;
+
+const ParagraphPurpose = styled.p`
+  width: 100%;
+  font-size: 22px;
+  font-weight: 700;
+  line-height: 30px;
+  color: #9e9e9e;
+`;
+
+const ImagePurpose = styled.img`
+  width: 700px;
+`;
+const ButtonEasier = styled.div`
+  background: linear-gradient(297.06deg , #f8ed84 23.88% , #f5e0ff 66.2% , #84fad5 109.31%);
+  border-radius: 43px;
+  height: 72px;
+  width: 350px;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TextButtonEasier = styled.span`
+  color: #262626;
+  font-size: 16px;
+  letter-spacing: 0.14em;
+`;
 
 const ProjectPurpose = () => {
 
@@ -8,23 +64,23 @@ const ProjectPurpose = () => {
         navigate('/signup');
     };
 
+
     return(
 
-            <div className='project-purpose'>
-                <div className='project-section-purpose '>
-                    <h1 className='heading-purpose'>Project Purpose</h1>
-                    <p className='paragraph-purpose '>
-                        Our main goal is to unite people from all over the world and help them fulfill their dreams.
-                        <br/>
-                        Working together in a friendly community for a brighter future.
-                    </p>
-                    <div type='submit' className='btn-easier' onClick={handleButtonClick}>
-                        <span className='text-button-easier'>Try now</span>
-                    </div>
-                </div>
-                <img src="https://dreamerz.net/static/media/myDream.8d70a4aefa9db11f815a.png" alt="dream" className='img-purpose'/>
-
-            </div>
+        <ProjectPurposeStyle>
+            <ProjectSectionPurpose>
+                <HeadingPurpose>Project Purpose</HeadingPurpose>
+                <ParagraphPurpose>
+                    Our main goal is to unite people from all over the world and help them fulfill their dreams.
+                    <br/>
+                    Working together in a friendly community for a brighter future.
+                </ParagraphPurpose>
+                <ButtonEasier onClick={handleButtonClick}>
+                    <TextButtonEasier>Try now</TextButtonEasier>
+                </ButtonEasier>
+            </ProjectSectionPurpose>
+            <ImagePurpose src="https://dreamerz.net/static/media/myDream.8d70a4aefa9db11f815a.png" alt="dream" />
+        </ProjectPurposeStyle>
 
     )
 }
