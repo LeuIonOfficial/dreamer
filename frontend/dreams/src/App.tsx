c895c7acc3c312380b9eb16fbbab9c429b8f5113
ionleu <leuionofficial@gmail.com>
2023-07-07 13:05:32 +0300

    added global styles for font

--- frontend/dreams/src/App.tsx
+++ frontend/dreams/src/App.tsx
@@ -5,8 +5,8 @@ import SignupScreen from "./screens/SignupScreen";
 import {createGlobalStyle} from "styled-components";
 import Easier from "./components/purpose/Easier";
 import ProjectPurpose from "./components/purpose/ProjectPurpose";
-import WingsDonation from "./components/Wings Donation/WingsDonation";
-import SecurelyDonation from "./components/Securely Donation/SecurelyDonation";
+import WingsDonation from "./components/WingsDonation/WingsDonation";
+import SecurelyDonation from "./components/SecurelyDonation/SecurelyDonation";
 
 
 
@@ -25,11 +25,9 @@ const App = () => {
         <BrowserRouter>
             <Routes>
                 {/*<Global/>*/}
-                {/*<Route path='/' element={<HomeScreen/>}/>*/}
+                <Route path='/' element={<HomeScreen/>}/>
                 <Route path='/login' element={<LoginScreen/>}/>
                 <Route path='/signup' element={<SignupScreen/>}/>
-                <Route path='/easier' element={<Easier/>}/>
-                <Route path='/purpose' element={<ProjectPurpose/>}/>
             </Routes>
         </BrowserRouter>
     )
