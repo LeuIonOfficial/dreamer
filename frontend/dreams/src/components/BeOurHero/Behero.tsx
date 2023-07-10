e9bc6f5fe9169b62ba5a707abc297cf92eb9823a
edward375 <rotarueduard91@gmail.com>
2023-07-10 13:49:51 +0300

    remove RBD and Behero style

--- frontend/dreams/src/components/BeOurHero/Behero.tsx
+++ frontend/dreams/src/components/BeOurHero/Behero.tsx
@@ -9,8 +9,10 @@ const ContainerBeHero = styled.div`
   justify-content: space-between;
   flex-direction: column;
   display: flex;
-  @media only screen and (width <= 800px) {
-    display: none;
+  @media (max-width: 800px) {
+    height: 320px;
+    border-bottom: 1px solid #98dfda;
+    padding: 15px;
   }
 `
 
@@ -23,99 +25,66 @@ const TitleBeHero = styled.h1`
   font-weight: 700;
   line-height: 65px;
   margin: 20px 30px;
+  color: #333;
+  @media (max-width: 800px) {
+    font-size: 32px;
+    line-height: 25px;
+    margin: 0;
+  }
 `
 
 const ContentBeHero = styled.div`
   display: flex;
   flex-direction: row;
-  font-family: monospace;
   margin: 10px 30px;
+  @media (max-width: 800px) {
+    display: flex;
+    flex-direction: column;
+    margin: 0;
+  }
 `
 
 const TextDescription = styled.p`
   font-size: 28px;
   line-height: 36px;
+  color: #060606;
+  @media (max-width: 800px) {
+    font-size: 16px;
+    line-height: 22px;
+    font-weight: 400;
+    margin: 0;
+  }
 `
 
 const TitleDescription = styled.h3`
   font-size: 28px;
   font-weight: 700;
   line-height: 36px;
-`
-const ContainerBeHeroMob = styled.div`
-  height: 350px;
-  border-bottom: 1px solid #98dfda;
-  padding: 38px;
-  box-sizing: border-box;
-  @media only screen and (width > 801px ) {
-    display: none;
+  color: #202020;
+  @media (max-width: 800px) {
+    font-size: 19px;
+    line-height: 41px;
+    margin: 0;
   }
 `
-
-const TitleBeHeroMob = styled.h1`
-  color: #333;
-  font-size: 32px;
-  font-weight: 700;
-  line-height: 25px;
-  margin: 0;
-`
-const ContainerDescriptionMob = styled.div`
-  margin-bottom: 15px;
-`
-const TitleDescriptionMob = styled.h3`
-  font-size: 19px;
-  font-weight: 700;
-  line-height: 41px;
-  margin: 0;
-  color: #202020;
-
-`
-const TextDescriptionMob = styled.p`
-  font-size: 16px;
-  line-height: 22px;
-  font-weight: 400;
-  color: #060606;
-  margin: 0;
-`
-
 const BeHero = () => {
     return (
-        <div>
-            <ContainerBeHero>
-                <TitleBeHero>Be our hero</TitleBeHero>
-                <ContentBeHero>
-                    <ContainerDescription>
-                        <TitleDescription>Dreamer</TitleDescription>
-                        <TextDescription>Dreamer is a person who gives for other dreams and receive back twice for it’s
-                            own
-                            dream.</TextDescription>
-                    </ContainerDescription>
-                    <ContainerDescription>
-                        <TitleDescription>Dream Angel</TitleDescription>
-                        <TextDescription>Dream Angel is a person who give to others and collect fulfilled dreams for his
-                            future dream or further donation.</TextDescription>
-                    </ContainerDescription>
-                </ContentBeHero>
-            </ContainerBeHero>
-
-            <ContainerBeHeroMob>
-                <TitleBeHeroMob>Be our hero</TitleBeHeroMob>
-                <div>
-                    <ContainerDescriptionMob>
-                        <TitleDescriptionMob>Dreamer</TitleDescriptionMob>
-                        <TextDescriptionMob>Dreamer is a person who gives for other dreams and receive back twice for
-                            it’s own
-                            dream.</TextDescriptionMob>
-                    </ContainerDescriptionMob>
-                    <ContainerDescriptionMob>
-                        <TitleDescriptionMob>Dream Angel</TitleDescriptionMob>
-                        <TextDescriptionMob>Dream Angel is a person who give to others and collect fulfilled dreams for
-                            his
-                            future dream or further donation.</TextDescriptionMob>
-                    </ContainerDescriptionMob>
-                </div>
-            </ContainerBeHeroMob>
-        </div>
+        <ContainerBeHero>
+            <TitleBeHero>Be our hero</TitleBeHero>
+            <ContentBeHero>
+                <ContainerDescription>
+                    <TitleDescription>Dreamer</TitleDescription>
+                    <TextDescription>Dreamer is a person who gives for other dreams and receive back twice for it’s
+                        own
+                        dream.</TextDescription>
+                </ContainerDescription>
+                <ContainerDescription>
+                    <TitleDescription>Dream Angel</TitleDescription>
+                    <TextDescription>Dream Angel is a person who give to others and collect fulfilled dreams for his
+                        future dream or further donation.</TextDescription>
+                </ContainerDescription>
+            </ContentBeHero>
+        </ContainerBeHero>
     )
 }
 
