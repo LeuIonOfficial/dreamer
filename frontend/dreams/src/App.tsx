f889e9ba2b79d24f9ef09735e8623fc2d830a2f5
Iuliana <lokygirl184@gmail.com>
2023-07-12 12:45:32 +0300

    Ready but not responsive

--- frontend/dreams/src/App.tsx
+++ frontend/dreams/src/App.tsx
@@ -5,20 +5,21 @@ import SignupScreen from "./screens/SignupScreen";
 import {Dashboard} from "./screens/Dashboard";
 import LeftSideBar from "./components/LeftSIdeBar/LeftSideBar";
 import SecurelyDonation from "./components/SecurelyDonation/SecurelyDonation";
-
+import ProfiePhotos from "./components/ProfilePhotos/ProfiePhotos";
 const App = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                {/*<Route path='/' element={<HomeScreen/>}/>*/}
-                {/*<Route path='/login' element={<LoginScreen/>}/>*/}
-                {/*<Route path='/signup' element={<SignupScreen/>}/>*/}
-                {/*<Route path='/dashboard' element={<Dashboard/>}/>*/}
-            </Routes>
-            <LeftSideBar />
-
-            {/*text*/}
-        </BrowserRouter>
+        <ProfiePhotos></ProfiePhotos>
+        // <BrowserRouter>
+        //     <Routes>
+        //         {/*<Route path='/' element={<HomeScreen/>}/>*/}
+        //         {/*<Route path='/login' element={<LoginScreen/>}/>*/}
+        //         {/*<Route path='/signup' element={<SignupScreen/>}/>*/}
+        //         {/*<Route path='/dashboard' element={<Dashboard/>}/>*/}
+        //     </Routes>
+        //     <LeftSideBar />
+        //
+        //     {/*text*/}
+        // </BrowserRouter>
     )
 }
 export default App
