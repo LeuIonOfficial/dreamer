4c20b4b3ba5c804a5ffcaade57d19c3c1c33fff2
ionleu <leuionofficial@gmail.com>
2023-07-21 14:12:52 +0300

    updated project

--- frontend/dreams/src/navigation/PrivateRoutes.tsx
+++ frontend/dreams/src/navigation/PrivateRoutes.tsx
@@ -1,5 +1,6 @@
 import {Outlet, Navigate} from "react-router-dom";
 import Logged from "../components/HeaderFooter/Logged";
+import Footer from "../components/HeaderFooter/Footer";
 
 export const PrivateRoutes = () => {
     const auth = localStorage.getItem("token")
